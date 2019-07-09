140d0b052dbbb7c4f14dc65594bbfb551fd9d8a7
lijie357 <lijie357@jd.com>
2019-07-09 18:15:09 +0800

    这是第二版啦

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,5 +1,16 @@
 public class HelloGit {
+
+    Git git;
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+    }
+
+    private class Git{
+        private String name;
+        private String time;
+        public Git(String name ,String time){
+            this.name=name;
+            this.time=time;
+        }
     }
 }
