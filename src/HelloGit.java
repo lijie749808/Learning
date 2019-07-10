c46215c72758d3769d416c579a28631bddee60b5
lijie357 <lijie357@jd.com>
2019-07-10 10:02:19 +0800

    这是V2.0

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -4,6 +4,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Nice to me too!");
+        //第二版2.0
+        System.out.println("This is second!");
     }
 
     private class Git{
